9d9ecae63ba927395e59b38cfc6bc0613b9ade7b
Ruslan Li <ruslan022015@gmail.com>
2021-11-29 18:39:17 +0600

    added identity

--- Hospital/Controllers/AccountController.cs
+++ Hospital/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Hospital.Models.ViewModels;
+using Hospital.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,14 +16,44 @@ namespace Hospital.Controllers
         /*IdentityUser - Представляет пользователя в системе удостоверений*/
         /* Предоставляет API-интерфейсы для управления пользователем в хранилище сохраняемости.*/
         private UserManager<IdentityUser> userManager;
-        /*Предоставляет API для входа пользователя.*/
+        /*Предоставляет API для входа пользователя. сервис SignInManager, который позволяет аутентифицировать пользователя и устанавливать или удалять его куки.*/
         private SignInManager<IdentityUser> signInManager;
         public AccountController(UserManager<IdentityUser> _userManager, SignInManager<IdentityUser> _signInManager)
         {
             userManager = _userManager;
             signInManager = _signInManager;
         }
-
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Hospital.Models.User user = new User { Email = model.Email, UserName = model.UserName, Year = model.Year};
+                /*Создает указанного пользователя в резервном хранилище с заданным паролем в качестве асинхронной операции.*/
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    /*устанавливаем аутентификационные куки для добавленного пользователя.*/
+                    
+                    await signInManager.SignInAsync(user, false);//BOOL variable надо ли хранить куки долго
+                    return RedirectToAction("MainPage", "Hospital");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);            
+        }
         /// <summary>
         /// 
         /// </summary>
